11b94fc910ed418dd0fa92e28491a7128b071cc4
Artem Artemev <artem@artemev.su>
2023-01-31 21:44:25 +0300

    taskCompleted

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿Console.WriteLine("Введите слова (символы, или числа) через запятую: ");
+string[] intArray = Console.ReadLine().Split(',');
+
+string[] filteredArray = new string[intArray.Length];
+int count = 0;
+for (int i = 0; i < intArray.Length; i++)
+{
+    if (intArray[i].Length <= 3)
+    {
+        filteredArray[count] = intArray[i];
+        count++;
+    }
+}
+Array.Resize(ref filteredArray, count);
+Console.WriteLine("Слова (символы, или числа) имеющие 3 или меньше значение: ");
+Console.WriteLine(string.Join(", ", filteredArray));
